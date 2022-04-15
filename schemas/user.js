94c61cd0e8b98bd5ee0c6b8fd9e804215c3d4c6f
tay yoon <dylanyoon92@gmail.com>
2022-04-15 16:14:11 +0900

    user Schema

--- schemas/user.js
+++ schemas/user.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 
-const UsersSchema = new mongoose.Schema({});
+const UsersSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+  },
+  follow: [String],
+  follower: [String],
+});
 
 // UserSchema.virtual('jwtId').get(function () {
 //     return this._id.toHexString();
