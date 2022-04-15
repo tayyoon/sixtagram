c6a77e5b6d0c1b67d835a6cfca57a940ec54dca3
tay yoon <dylanyoon92@gmail.com>
2022-04-15 15:48:30 +0900

    auth 추가

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const User = require("../schemas/user");
+const fs = require("fs");
+const mykey = fs.readFileSync(__dirname + "/key.txt").toString();
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.status(401).send({
+      errorMEssage: "로그인 후 사용하세요",
+    });
+    return;
+  }
+  const [tokenType, tokenValue] = authorization.split(" ");
+
+  if (tokenType !== "Bearer") {
+    res.status(401).send({
+      errorMessage: "로그인 후 사용하세요!",
+    });
+    return;
+  }
+
+  //jwt검증//
+  try {
+    const { userId } = jwt.verify(tokenValue, mykey);
+    //검증 성공시 locals에 인증 정보 넣어주기//
+    console.log(userId);
+    User.findOne({ userId })
+      .exec()
+      .then((user) => {
+        res.locals.user = user;
+        next();
+      });
+  } catch (error) {
+    res.status(401).send({
+      errorMEssage: "로그인 하시고 사용하세요",
+    });
+    return;
+  }
+};
