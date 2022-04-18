b47cd02779cf975e0b4d161f8431eda78c5b1064
tay yoon <dylanyoon92@gmail.com>
2022-04-18 20:44:39 +0900

    post 조회 수정

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Post = require("../schemas/post");
+const User = require("../schemas/user");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const moment = require("moment");
@@ -10,34 +11,10 @@ const fs = require("fs");
 require("dotenv").config();
 const authMiddleware = require("../middlewares/auth-middleware");
 
-// 게시글 목록 조회
-// router.get("/blogList", async (req, res, next) => {
-
-//   try {
-//     const blogList = await Blog.find({}).sort("-NowDate");
-//     res.json({ blogList });
-//   } catch (err) {
-//     console.error(err);
-//     next(err);
-//   }
-// });
-
-//게시글조회 페이지
-router.get("/postList/:PostId", async (req, res) => {
-  //주소에 PostId를 파라미터값으로 가져옴
-  const { PostId } = req.params;
-  //console.log(PostId); //ok
-
-  idList = await Post.findOne({ PostId });
-  //detail 값으로 넘겨줌
-  res.json({ idList });
-});
-
 const path = require("path");
 let AWS = require("aws-sdk");
 AWS.config.loadFromPath(path.join(__dirname, "../config/s3.json")); // 인증
 let s3 = new AWS.S3();
-
 let multer = require("multer");
 let multerS3 = require("multer-s3");
 let upload = multer({
@@ -52,31 +29,28 @@ let upload = multer({
   }),
 });
 
-// 게시글 조회 //follow 리스트 있는 id만 보이게 하기
+// 게시글 조회 //follow 리스트 있는 id의 글만 보이게 하기
 router.get("/postList", authMiddleware, async (req, res, next) => {
-  //const { user } = res.locals;
-  const { idList } = req.body;
-  //console.log(idList)  //ok
+  const { user } = res.locals;
+  const idList = req.body;
 
-  // 현재 로그인된 사용자의 follow 확인용.
-  // const { user } = res.locals;
-  // console.log(user)  //ok // test02의 follw는 test04이다.
+  //console.log(idList)
+  const followPost = [];
 
   try {
-    // for(const followId in idList){
-    //   console.log(followId)
-    //   const postList = await Post.find({ userId : followId })
-    //   res.json({postList});
-    // }
-
     for (i = 0; i < idList.length; i++) {
       let followId = idList[i];
-      console.log(followId); //ok
+      //console.log(followId)  //ok //
       const postList = await Post.find({ userId: followId });
-      console.log(postList); // ok //follow의 글만 조회됌
-      //.sort("-createdAt");
-      res.json({ postList });
+      for (j = 0; j < postList.length; j++) {
+        followPost.push(postList[j]);
+      }
     }
+    followPost.sort(followPost.createdAt).reverse();
+    console.log(followPost);
+    // friendsinfo.sort(friendsinfo.createdAt);
+    // console.log("aa",friendsinfo)
+    return res.json({ followPost });
   } catch (err) {
     res.status(400).json({ msg: "게시글이 조회되지 않았습니다." });
     next(err);
@@ -94,17 +68,14 @@ router.post(
     const imageUrl = req.file.location;
     //console.log("req.file: ", req.file); // 테스트 => req.file.location에 이미지 링크(s3-server)가 담겨있음
     console.log(content, imageUrl);
-
     // 사용자 브라우저에서 보낸 쿠키를 인증미들웨어통해 user변수 생성
     const { user } = res.locals;
     const userId = user.userId;
-    console.log(user); //ok
-
+    // console.log(user)  //ok
     // 글작성시각 생성
     require("moment-timezone");
     moment.tz.setDefault("Asia/Seoul");
     const createdAt = String(moment().format("YYYY-MM-DD HH:mm:ss"));
-
     try {
       const postList = await Post.create({
         imageUrl,
@@ -129,13 +100,11 @@ router.patch(
     const { content } = req.body;
     const imageUrl = req.file.location;
     //console.log(userId) //ok
-
     //게시글 내용이 없으면 저장되지 않고 alert 뜨게하기.
     if (!content.length) {
       res.status(401).send({ msg: "게시글 내용을 입력해주세요." });
       return;
     }
-
     try {
       const video = await Post.find({ _id: postId }); // 현재 URL에 전달된 id값을 받아서 db찾음
       const url = video[0].imageUrl.split("/"); // video에 저장된 fileUrl을 가져옴
@@ -151,10 +120,8 @@ router.patch(
           }
         }
       );
-
       await Post.updateOne({ _id: postId }, { $set: { content, imageUrl } });
       const postList = await Post.findOne({ _id: postId });
-
       res.send({ result: "success", postList });
     } catch {
       res.status(400).send({ msg: "게시글이 수정되지 않았습니다." });
@@ -167,13 +134,10 @@ router.delete("/posts/:postId", authMiddleware, async (req, res) => {
   const { postId } = req.params;
   const video = await Post.find({ _id: postId }); // 현재 URL에 전달된 id값을 받아서 db찾음
   //console.log(postId)
-
   const url = video[0].imageUrl.split("/"); // video에 저장된 fileUrl을 가져옴
   const delFileName = url[url.length - 1];
-
   try {
     await Post.deleteOne({ _id: postId });
-
     s3.deleteObject(
       {
         Bucket: "sixtagram",
@@ -190,7 +154,6 @@ router.delete("/posts/:postId", authMiddleware, async (req, res) => {
     res.status(400).send({ msg: "게시글이 삭제되지 않았습니다." });
   }
 });
-
 router.get("/", (req, res) => {
   res.send("this is root page");
 });
